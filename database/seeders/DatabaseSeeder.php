4ff741cf74b2652542b12829f70573951ae02a55
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2024-03-18 23:02:13 +0700

    Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +21,65 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Ahmad Muhyiddin',
+            'email' => 'muhyi@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Bang Yi',
+            'email' => 'bangyi@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam vitae laborum sed in! Impedit eum, repellat odit nihil',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam vitae laborum sed in! Impedit eum, repellat odit nihil consequatur animi. Repellat, necessitatibus modi quod ratione nam repudiandae corrupti itaque porro aut voluptatibus voluptates qui eius recusandae quasi nesciunt ab deserunt rem tempora quos debitis perspiciatis! Aut accusantium odit assumenda, est ad eius tempora sunt illum corporis, dolores placeat obcaecati natus eveniet nemo porro deserunt! Amet labore repudiandae praesentium fuga obcaecati, numquam adipisci consequatur alias. Incidunt delectus minima officia ullam aut exercitationem praesentium ipsum sint minus adipisci tempora necessitatibus, ducimus eaque nobis nemo dicta laborum facilis corrupti sapiente saepe maiores.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Dua',
+            'slug' => 'judul-ke-dua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam vitae laborum sed in! Impedit eum, repellat odit nihil',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam vitae laborum sed in! Impedit eum, repellat odit nihil consequatur animi. Repellat, necessitatibus modi quod ratione nam repudiandae corrupti itaque porro aut voluptatibus voluptates qui eius recusandae quasi nesciunt ab deserunt rem tempora quos debitis perspiciatis! Aut accusantium odit assumenda, est ad eius tempora sunt illum corporis, dolores placeat obcaecati natus eveniet nemo porro deserunt! Amet labore repudiandae praesentium fuga obcaecati, numquam adipisci consequatur alias. Incidunt delectus minima officia ullam aut exercitationem praesentium ipsum sint minus adipisci tempora necessitatibus, ducimus eaque nobis nemo dicta laborum facilis corrupti sapiente saepe maiores.',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Tiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam vitae laborum sed in! Impedit eum, repellat odit nihil',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam vitae laborum sed in! Impedit eum, repellat odit nihil consequatur animi. Repellat, necessitatibus modi quod ratione nam repudiandae corrupti itaque porro aut voluptatibus voluptates qui eius recusandae quasi nesciunt ab deserunt rem tempora quos debitis perspiciatis! Aut accusantium odit assumenda, est ad eius tempora sunt illum corporis, dolores placeat obcaecati natus eveniet nemo porro deserunt! Amet labore repudiandae praesentium fuga obcaecati, numquam adipisci consequatur alias. Incidunt delectus minima officia ullam aut exercitationem praesentium ipsum sint minus adipisci tempora necessitatibus, ducimus eaque nobis nemo dicta laborum facilis corrupti sapiente saepe maiores.',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ke Empat',
+            'slug' => 'judul-ke-empat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam vitae laborum sed in! Impedit eum, repellat odit nihil',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam quisquam vitae laborum sed in! Impedit eum, repellat odit nihil consequatur animi. Repellat, necessitatibus modi quod ratione nam repudiandae corrupti itaque porro aut voluptatibus voluptates qui eius recusandae quasi nesciunt ab deserunt rem tempora quos debitis perspiciatis! Aut accusantium odit assumenda, est ad eius tempora sunt illum corporis, dolores placeat obcaecati natus eveniet nemo porro deserunt! Amet labore repudiandae praesentium fuga obcaecati, numquam adipisci consequatur alias. Incidunt delectus minima officia ullam aut exercitationem praesentium ipsum sint minus adipisci tempora necessitatibus, ducimus eaque nobis nemo dicta laborum facilis corrupti sapiente saepe maiores.',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
+
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,11 +4,11 @@
     {{-- <article> --}}
         <h2 class="mb-5">{{ $post->title }}</h2>
 
-        <p>By. Ahmad Muhyi in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+        <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
 
         {{-- <h5>{{ $post["author"] }}</h5> --}}
         {!! $post->body !!}
     {{-- </article> --}}
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/blog" class="d-block mt-3">Back to Posts</a>
 @endsection
